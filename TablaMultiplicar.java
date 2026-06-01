def1bad8e0e292f3dcdd2361aa63971e324a27d5
jpg-ms-yt <tinovalegomez@gmail.com>
2026-06-01 10:06:32 -0500

    Add files via upload

---
+++ TablaMultiplicar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Ingresa un número para ver su tabla de multiplicar: ");
+        int numero = entrada.nextInt();
+        
+        System.out.println("--- Tabla del " + numero + " ---");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " × " + i + " = " + (numero * i));
+        }
+    }
+}
